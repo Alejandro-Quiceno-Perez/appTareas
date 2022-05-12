9e68676416dec4beb8259d83c817fca8844bd4e2
Alejandro Quiceno Pérez <alejo.perez2905@gmail.com>
2022-05-11 21:59:21 -0500

    Sse crearon las funcionalidades con js para nuestra app de tareas

--- app.js
+++ app.js
@@ -22,9 +22,45 @@ function saveTask(e) {
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 
-    e.preventDefault()
+    getTasks();
+    document.getElementById('formTask').reset();
+    e.preventDefault();
 }
 
+function getTasks() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let tasksView = document.getElementById('tasks');
+
+    tasksView.innerHTML = '';
+
+    for (let i = 0; i < tasks.length; i++) {
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+
+
+        tasksView.innerHTML += ` <div class="card mb-2">
+            <div class="card-body">
+                <p>${title} - ${description}</p>
+                <a class="btn btn-danger" onclick="deleteTask('${title}')">Eliminar</a>
+            </div>
+        </div> `
+
+    }
+}
+
+function deleteTask(title) {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].title == title) {
+            tasks.splice(i, 1);
+        }
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    getTasks();
+}
+
+getTasks();
 
 
 
